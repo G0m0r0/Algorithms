c079e5735811470683f90ceb3391730d5210a80d
Delyan Dimitrov <dimitrovdelyann@gmail.com>
2021-11-17 21:12:02 +0200

    min max heap data structure in c#

---
+++ HeapStructure/HeapStructure/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HeapStructure
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            //binary heap tree
+            var maxHeap = new MaxHeap(new int[] { 1, 2, 3, 4 }, 4);
+            var mminHeap = new MinHeap(new int[] { 1, 2, 3, 4 }, 4);
+        }
+    }
+}
