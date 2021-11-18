cebfc447a4347cf6f811bc78c79a4999105b270e
Delyan Dimitrov <dimitrovdelyann@gmail.com>
2021-11-18 21:57:05 +0200

    edition/CheckIfStrIsPermutationOfAnother/bin/Debug/net5.0/CheckIfStrIsPermutationOfAnother.runtimeconfig.dev.jsonche# new file:   Cracking the code interview 6th edition/CheckIfStrIsPermutationOfAnother/bin/Debug/net5.0/CheckIfStrIsPermutationOfAnother.runtimeconfig.json

---
+++ Cracking the code interview 6th edition/CheckIfStrIsPermutationOfAnother/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CheckIfStrIsPermutationOfAnother
+{
+    class Program
+    {
+        static List<string> allPermutations = new List<string>();
+        static void Main()
+        {
+            //Check Permutation: Given two strings, write a method to decide if one is a permutation of the other
+            var str1 = "abc";
+            var str2 = "cba";
+            var isPermutation = false;
+
+            if (str1.Length != str2.Length)
+            {
+                isPermutation = false;
+            }
+            else
+            {
+                var str1Ordered = string.Join("", str1.OrderBy(x => x));
+                var str2Ordered = string.Join("", str2.OrderBy(x => x));
+
+                if (str1Ordered == str2Ordered)
+                {
+                    isPermutation = true;
+                }
+            }
+
+            Console.WriteLine(isPermutation);
+        }
+    }
+}
---
+++ Cracking the code interview 6th edition/UniqueCharacterInString/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace UniqueCharacterInString
+{
+    class Program
+    {
+        static void Main()
+        {
+            //look if all characters in are unique
+            //impliment without additional structures
+            string str = "abcdef5.";
+
+            //O(n^2)
+            var isUnique = true;
+            // for (int i = 0; i < str.Length; i++)
+            // {
+            //     for (int j = i+1; j < str.Length; j++)
+            //     {
+            //         if (str[i] == str[j])
+            //         {
+            //             isUnique = false;
+            //         }
+            //     }
+            // }
+
+            if (str.Length > 256) //is case of ASCII
+            {
+                isUnique = false;
+            }
+            else
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (str.Substring(i + 1).Contains(str[i]))
+                    {
+                        isUnique = false;
+                    }
+                }
+            }
+
+            Console.WriteLine(isUnique);
+        }
+    }
+}
