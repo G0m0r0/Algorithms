735afcc405606746af34d58f3b52e9f87925f683
G0m0r0 <dimitrovdelyann@gmail.com>
2022-08-12 19:03:43 +0300

    empty commit

---
+++ recursion/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Queue<int> a = new Queue<int>();
+            a.Enqueue(9);
+            a.Enqueue(6);
+            a.Enqueue(8);
+            a.Enqueue(4);
+            a.Enqueue(5);
+            a.Enqueue(0);
+            a.Enqueue(3);
+
+            a.Dequeue();
+            var b = a.ToList();
+            b.Sort();
+
+            Console.WriteLine(String.Join(" ", b));
+        }
+    }
+}
